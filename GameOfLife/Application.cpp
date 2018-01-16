ed7fe4b222a4ec69c66eb893c01dc4b64cca4772
Nabeel Siddiqui <dninja797@gmail.com>
2018-01-16 22:29:03 +0400

    Added A menu and optional start

--- GameOfLife/Application.cpp
+++ GameOfLife/Application.cpp
@@ -5,17 +5,24 @@
 Application::Application() {
 	m_mainWindow.create(sf::VideoMode(m_cellWidth * (m_cellNumX - 2), m_cellWidth * (m_cellNumY - 2)), "Conway's Game Of Life", sf::Style::Close);
 	srand(time(NULL));
+	
 	for (unsigned short int x = 0; x < m_cellNumX; x++) {
 		for (unsigned short int y = 0; y < m_cellNumY; y++) {
 			m_cells[x][y] = new Cell({ (x * m_cellWidth) - m_cellWidth, (y * m_cellWidth) - m_cellWidth }, { m_cellWidth, m_cellWidth });
-			if (x == 0 || x == m_cellNumX - 1 || y == 0 || y == m_cellNumY - 1) {
-				// makes dead cells
-			}
-			else if (rand() % 100 < 25) {
-				m_cells[x][y]->SetState(ALIVE);
-			}
 		}
 	}
+
+	m_font.loadFromFile("./fonts/fancy.ttf");
+	m_generationText.setFont(m_font);
+	m_generationText.setPosition({0.0f, 0.0f});
+	m_generationText.setCharacterSize(15u);
+	m_generationText.setFillColor(sf::Color(15, 15, 255));
+
+	m_promptText.setFont(m_font);
+	m_promptText.setString("Press E to manually generate live cells OR \n press R to randomly generate live cells");
+	m_promptText.setPosition({ 50.0f, 10.0f });
+	m_promptText.setCharacterSize(25u);
+	m_promptText.setFillColor(sf::Color(15, 105, 255));
 }
 
 
@@ -23,19 +30,52 @@ void Application::Run() {
 	while (m_mainWindow.isOpen()) {
 		sf::Event evnt;
 		while (m_mainWindow.pollEvent(evnt)) {
+			this->pollEvent(evnt);
 			if (evnt.type == sf::Event::Closed) {
 				m_mainWindow.close();
 			}
 		}
 		m_mainWindow.clear();
 
-		if (m_deltaTime.getElapsedTime().asMilliseconds() > 100) {
-			this->Update();
-			m_deltaTime.restart();
+		if (m_hasBegun) {
+			if (m_deltaTime.getElapsedTime().asMilliseconds() > 100) {
+				this->Update();
+				m_deltaTime.restart();
+			}
 		}
 		this->Render();
 
 		m_mainWindow.display();
+	}
+}
+
+void Application::pollEvent(sf::Event& evnt) {
+	switch (evnt.type) {
+	case sf::Event::KeyPressed:
+		if (!m_hasBegun) { // these cant run during the simulation
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::E)) {
+				m_mode = EDIT;
+				// editable (will add)
+			}
+			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::R)) {
+				m_mode = RANDOM;
+				this->KillAll();
+				this->SetRandomToAlive();
+			}
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Return)) {
+			if (m_hasBegun) { // if the simulation stopped
+				this->KillAll();
+				m_mode = NONE;
+			}
+
+			if (m_mode != NONE || m_hasBegun) {
+				m_hasBegun = !m_hasBegun;
+				m_generation = 0;
+				m_generationText.setString(""); // make generation text disappear
+			}
+		}
+		break;
 	}
 }
 
@@ -61,13 +101,39 @@ void Application::Update() {
 			}
 		}
 	}
-	SpawnAndKill();
+	SpawnAndKill(); // the cells are spawned and killed once a generation
+
+	m_generation++;
+	m_generationText.setString("Generation: " + std::to_string(m_generation));
 }
 
 void Application::Render() {
 	for (unsigned short int x = 1; x < m_cellNumX - 1; x++) {
 		for (unsigned short int y = 1; y < m_cellNumY - 1; y++) {
 			m_cells[x][y]->Render(m_mainWindow);
+		}
+	}
+	m_mainWindow.draw(m_generationText);
+	if (m_mode == NONE) m_mainWindow.draw(m_promptText);
+}
+
+void Application::SetRandomToAlive() {
+	for (unsigned short int x = 0; x < m_cellNumX; x++) {
+		for (unsigned short int y = 0; y < m_cellNumY; y++) {
+			if (x == 0 || x == m_cellNumX - 1 || y == 0 || y == m_cellNumY - 1) {
+				// makes dead cells
+			}
+			else if (rand() % 100 < 25) {
+				m_cells[x][y]->SetState(ALIVE);
+			}
+		}
+	}
+}
+
+void Application::KillAll() {
+	for (unsigned short int x = 0; x < m_cellNumX; x++) {
+		for (unsigned short int y = 0; y < m_cellNumY; y++) {
+			m_cells[x][y]->SetState(DEAD);
 		}
 	}
 }
--- GameOfLife/Application.h
+++ GameOfLife/Application.h
@@ -3,6 +3,12 @@
 #include <iostream>
 #include "Cell.h"
 
+enum Mode {
+	NONE,
+	EDIT,
+	RANDOM
+};
+
 class Application
 {
 public:
@@ -14,22 +20,38 @@ private:
 	void Update();
 	void Render();
 
+	void SetRandomToAlive();
+	void KillAll();
+
 	int CountNeighbors(const int Cx, const int Cy) const;
 
 	void AddToDeathList(int x, int y);
 	void AddToBirthList(int x, int y);
 
 	void SpawnAndKill();
+
+	void pollEvent(sf::Event& evnt);
 private:
 	sf::RenderWindow m_mainWindow;
+
 	Cell* m_cells[72][44];
+
 	sf::Clock m_deltaTime;
+
+	Mode m_mode = NONE;
 
 	std::vector<sf::Vector2i> m_killCoords;
 	std::vector<sf::Vector2i> m_birthCoords;
 
+	unsigned long long int m_generation = 0;
+	sf::Font               m_font;
+	sf::Text               m_generationText;
+	sf::Text               m_promptText;
+
 	const int m_cellNumX = 72;
 	const int m_cellNumY = 44;
 	const int m_cellWidth = 15;
+
+	bool m_hasBegun = false;
 };
 
